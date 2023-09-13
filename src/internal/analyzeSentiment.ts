14cabf07ec4793932a392e9a9db522139d17e789
Carolyne Mbulwa <cmbulwa@microsoft.com>
2023-09-13 17:42:36 +0300

    added text analysis

---
+++ src/internal/analyzeSentiment.ts
@@ -0,0 +1,8 @@
+import { env } from "process";
+import { client } from "../openAiClient";
+
+export async function analyzeSentiment(textToAnalyze: string) {
+  const completion = await client.getChatCompletions("gpt-35-turbo-16k", [{content: `Reply with either positive or negative with regards to this sentiment:'${textToAnalyze}'`, role: 'assistant' }]);
+  return completion;
+  
+}
