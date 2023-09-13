0cc5f82cee170dfac24e217c4f81a1aa8498b728
Clare Limo <limoclare@microsoft.com>
2023-09-13 19:03:54 +0300

    added rephrase function

--- src/teamsBot.ts
+++ src/teamsBot.ts
@@ -1,9 +1,14 @@
-import { TeamsActivityHandler } from "botbuilder";
+import { ActivityHandler } from "botbuilder";
 
 // An empty teams activity handler.
 // You can add your customization code here to extend your bot logic if needed.
-export class TeamsBot extends TeamsActivityHandler {
+export class TeamsBot extends ActivityHandler {
   constructor() {
     super();
+
+    this.onMessage(async (context, next) => {
+      console.log("The message context", context.activity);
+      console.log("The message context", context.activity.type);
+    })
   }
 }
