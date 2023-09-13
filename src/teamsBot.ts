af191e03c1db8b6a54b892899e5802f7d22c44b1
Clare Limo <limoclare@microsoft.com>
2023-09-13 19:25:54 +0300

    message successfully rephrased

--- src/teamsBot.ts
+++ src/teamsBot.ts
@@ -1,4 +1,6 @@
+
 import { ActivityHandler } from "botbuilder";
+import { rephraseMessage } from "./internal/analyzeSentiment";
 
 // An empty teams activity handler.
 // You can add your customization code here to extend your bot logic if needed.
@@ -7,8 +9,10 @@ export class TeamsBot extends ActivityHandler {
     super();
 
     this.onMessage(async (context, next) => {
-      console.log("The message context", context.activity);
-      console.log("The message context", context.activity.type);
+      console.log("The message context", JSON.stringify(context.activity.value, null, 2));
+      const rephrased = await rephraseMessage(context.activity.value.value);
+
+      console.log("after rephrasing", JSON.stringify(rephrased, null,2));
     })
-  }
+  }   
 }
