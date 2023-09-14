d49813fed019a9b291117c6412fc3f7ff2152b6d
Bolton Opondo <boltonopondo@microsoft.com>
2023-09-14 14:58:12 +0300

    Add Rephrase Card

--- src/teamsBot.ts
+++ src/teamsBot.ts
@@ -1,6 +1,47 @@
-
-import { ActivityHandler } from "botbuilder";
+import { ActivityHandler, CardFactory } from "botbuilder";
 import { rephraseComment, translateComment } from "./internal/gptActions";
+
+const resultOutputCard = ({
+  title,
+  content,
+}: {
+  title: string;
+  content: string;
+}) => ({
+  type: "AdaptiveCard",
+  body: [
+    {
+      type: "TextBlock",
+      size: "Medium",
+      weight: "Bolder",
+      text: title,
+      wrap: true,
+    },
+    {
+      type: "Container",
+      items: [
+        {
+          type: "TextBlock",
+          text: content,
+          wrap: true,
+          separator: true,
+        },
+      ],
+      style: "emphasis",
+    },
+    {
+      type: "TextBlock",
+      horizontalAlignment: "Left",
+      color: "Warning",
+      text: "⚠️ This AI-generated response may contain inaccuracies",
+      wrap: true,
+      size: "Small",
+      weight: "Lighter",
+    },
+  ],
+  $schema: "http://adaptivecards.io/schemas/adaptive-card.json",
+  version: "1.6",
+});
 
 // An empty teams activity handler.
 // You can add your customization code here to extend your bot logic if needed.
@@ -12,8 +53,13 @@ export class TeamsBot extends ActivityHandler {
       console.log(context.activity.value.event, "Context");
       switch (context.activity.value.event) {
         case "rephrase":
+          const title = "Rephrasing is complete ✅";
           const rephrased = await rephraseComment(context.activity.value.value);
-          context.sendActivity(rephrased.choices[0].message.content);
+          const content = rephrased.choices[0].message.content;
+          const card = CardFactory.adaptiveCard(
+            resultOutputCard({ title, content })
+          );
+          context.sendActivity({ attachments: [card] });
           next();
           break;
 
